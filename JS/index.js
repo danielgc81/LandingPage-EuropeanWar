d9c683fb552ade2b00e5ce44f47601a669078c77
Daniel Garrido <danielgc81@educastur.es>
2025-02-16 11:39:50 +0100

    Presentacion del proyecto y developers

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+document.addEventListener ("scroll", () => {
+   let maxHeight = window.document.body.scrollHeight - window.innerHeight;
+   let percentage = ((window.scrollY) / maxHeight) * 100;
+   document.querySelector(".scroll__line").style.width = percentage + '%';
+});
